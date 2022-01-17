646a049865030d01bcd66c4cdb32fc4c7a1dbe70
BeZaBell <azamorabell@gmail.com>
2022-01-17 15:23:09 -0600

    fish config installation script

--- install_fish.sh
+++ install_fish.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+if [[ ! -d $HOME/.config/fish ]]
+then
+    mkdir $HOME/.config/fish
+    if [[ ! -d $HOME/.config/fish/functions ]]
+    then
+        mkdir $HOME/.config/fish/functions
+    fi
+fi
+
+echo "Creating fish config directory and functions directory"
+
 cp fish/fish_greeting.fish $HOME/.config/fish/functions/
 cp fish/fish_prompt.fish $HOME/.config/fish/functions/
 cp fish/vim.fish $HOME/.config/fish/functions/
