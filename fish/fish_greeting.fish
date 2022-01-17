2c5d5ed3939b29684152543aa5c451d4ca7edfde
BeZaBell <azamorabell@gmail.com>
2022-01-17 00:35:01 -0600

    fish stuff

---
+++ fish/fish_greeting.fish
@@ -0,0 +1,4 @@
+function fish_greeting
+	echo -e "\033[3mThe agency that arises with life brings value, meaning, and action into the universe.\033[0m"
+	echo -e "	\033[1mStuart A. Kaufmann.\033[0m"
+end
---
+++ install_fish.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp fish_greeting.fish $HOME/.config/fish/functions/
+cp fish_prompt.fish $HOME/.config/fish/functions/
+cp vim.fish $HOME/.config/fish/functions/
